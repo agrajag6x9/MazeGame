ec13ce2eb6a9508e746f55e8cecdb5b420df9e01
Ben W <agrajag6x9@gmail.com>
2014-11-09 23:21:48 -0500

    Audio Things!
    Added a couple of Audio tracks to the game.  As a proof of concept, you
    can switch between them by pressing space.  Also added some directories
    for different assets.

---
+++ MazeGame/Assets/scripts/AudioManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AudioManager : MonoBehaviour {
+
+	// define the different audio states
+	// (normal): the default exploration theme
+	// (chase): the theme for when the player is being chased
+	enum State {
+		normal,
+		chase
+	}
+	
+	// define the variables
+	public AudioClip normalClip;
+	public AudioClip chaseClip;
+	
+	private AudioSource asrc;
+	private State curState;
+	
+	// Use this for initialization
+	void Start () {
+		asrc = GetComponent<AudioSource>() as AudioSource;
+		asrc.clip = normalClip;
+		asrc.Play ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if ( Input.GetButtonDown ("Jump") ) {
+			if (asrc.clip == normalClip) {
+				PlayChase ();
+			} else {
+				PlayNormal ();
+			}
+		}
+	}
+	
+	
+	void PlayNormal() {
+		asrc.clip = normalClip;
+		asrc.Play ();
+	}
+	
+	void PlayChase() {
+		asrc.clip = chaseClip;
+		asrc.Play ();
+	}
+}
